1c1a83681ae57078e3df4d27d768b7c3509b85a5
Bryan Santos Avalos <115680288+BryanSantosAv@users.noreply.github.com>
2025-03-26 17:52:30 -0500

    avance header, footer y page principal

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,19 @@
+import styles from "./home.module.css";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/Footer";
-
+import SectionPrincipal from "../../pages/sectionPrincipal/SectionPrincipal";
 
 export const Home = () => {
     
  return (
     <>
     <Header/>  
-    
+
+    <div id="principal">
+      <SectionPrincipal/>
+    </div>
+
    
-
-
     <Footer/>
     </>
  )
---
+++ src/pages/sectionPrincipal/SectionPrincipal.jsx
@@ -0,0 +1,29 @@
+import styles from "./SectionPrincipal.module.css";
+
+const SectionPrincipal = () => {
+
+  return (
+    <div className={styles.carrusel}>
+        <div className={styles.contenido}>
+            <img src="/src/assets/image/logo-navi.png" className={styles.logo}/>
+            <h1 className={styles.titulo}>
+            ¡Renueva tu Belleza!
+            </h1>
+            <p className={styles.descripcion}>
+                Descubre nuestras promociones especiales en cuidado de la piel
+                y maquillaje. ¡Solo por tiempo limitado!
+            </p>
+
+            <div className={styles.buttons}>
+                <button className={styles.promobutton}>Ver Promoción</button>
+                <button className={styles.conocebutton}>Conócenos</button>
+            </div>
+
+            </div>
+            <img src="/src/assets/image/mujer.png" className={styles.model} alt="Modelo"/>
+    </div>
+  );
+
+};
+
+export default SectionPrincipal;
