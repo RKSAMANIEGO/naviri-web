60b5b6708df3a5092600f3c6190a8a85253c950b
EdwinPoma <pomachicomaedwin@gmail.com>
2025-03-26 16:29:00 -0500

    jalando cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer'
+import Formulario from './components/Formulario'
+import TestimonioPrincipal from './components/TestimonioPrincipal'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <TestimonioPrincipal></TestimonioPrincipal>
+      <Formulario></Formulario>
+      <Footer></Footer>
+
     </>
   )
 }
