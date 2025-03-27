e9718aabcfde433254c3aa5fcdc17c34c6a1d9e3
EdwinPoma <pomachicomaedwin@gmail.com>
2025-03-27 13:34:41 -0500

    jalando cambios

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/Footer";
-
-
+import Formulario from '../SectionFormulario/Formulario';
+import Testimonios from '../SectionTestimonios/TestimonioPrincipal'
 export const Home = () => {
     
  return (
     <>
     <Header/>  
+      <div id="testimonio">
+         <Testimonios/>
+      </div>
     
-   
+      <div id="formulario">
+         <Formulario/>
+      </div>
 
 
     <Footer/>
