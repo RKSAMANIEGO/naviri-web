84e38d477282c67b70f99d85d40219d826f7eeba
Bryan Santos Avalos <115680288+BryanSantosAv@users.noreply.github.com>
2025-03-28 12:06:32 -0500

    avance dropdown

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,15 @@
-import {FaShoppingCart, FaSearch} from 'react-icons/fa';
+import { useState } from 'react';
+import {FaShoppingCart, FaSearch, FaChevronDown } from 'react-icons/fa';
 import styles from "../../styles/header.module.css";
 
 const Header = () => {
+
+    const [showProductos, setShowProductos] = useState(false);
+    const [showCategorias, setShowCategorias] = useState(false);
+
+
     return (
+
     <header className={styles.header}>
         <div className={styles.logo}>
             <img src="/src/assets/image/logo-navi.png" alt="logo"/>
@@ -10,12 +17,48 @@ const Header = () => {
 
         <nav className={styles.nav}>
             <a href="#">Inicio</a>
-            <div className={styles.dropnow}>
-                <a href="#">Productos</a>
+
+            <div className={styles.dropnow}
+                onMouseEnter={() => setShowProductos(true)}
+                onMouseLeave={() => setShowProductos(false)}
+            >
+                <a href="#">Productos <FaChevronDown className={styles.iconDropdown}/></a>
+                {showProductos && (
+                    <div className={styles.dropdown}>
+                        <a href="#" className={styles.productosnew}>Nuevos Productos
+                            <p>Descubre nuestras ultimas novedades en productos de belleza</p>
+                        </a>
+                        <a href="#">Ver Todo
+                            <p>Explora nuestra colección completa de productos</p>
+                        </a>
+                        <a href="#">Ciudado facial
+                            <p>Cremas, sérums, mascarillas y más para tu rutina facial.</p>
+                        </a>
+                        <a href="#">Maquillaje
+                            <p>Bases, labiales, sombras y todo para tu look perfecto</p>
+                        </a>
+                        <a href="#">Ciudado capilar
+                            <p>Champús, acondicionadores y tratamientos para tu cabello</p>
+                        </a>
+                    </div>
+                )}
             </div>
 
-            <div className={styles.dropnow}>
-                <a href="#">Categorías</a>
+            <div className={styles.dropnow}
+                onMouseEnter={() => setShowCategorias(true)}
+                onMouseLeave={() => setShowCategorias(false)}
+            >
+                <a href="#">Categorías <FaChevronDown className={styles.iconDropdown}/></a>
+                {showCategorias && (
+                    <div className={styles.dropdown}>
+                        <a href="#">Accesorios</a>
+                        <a href="#">Aceites</a>
+                        <a href="#">Cosmeticos</a>
+                        <a href="#">Ciudado capilar</a>
+                        <a href="#">Ciudado corporal</a>
+                        <a href="#">Sales Minerales</a>
+                    </div>
+                )}
             </div>
             
             <a href="#">Nosotros</a>
