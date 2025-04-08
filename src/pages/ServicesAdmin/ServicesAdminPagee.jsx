d7042d39840d256b396e1c53cd7c6b0d9ef297ef
Bryan Santos Avalos <115680288+BryanSantosAv@users.noreply.github.com>
2025-04-08 17:49:14 -0500

    avance table services

---
+++ src/pages/ServicesAdmin/ServicesAdminPagee.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServicesAdminPage from '../../pages/ServicesAdmin/ServicesAdminpage';
+
+const ServiceAdminPagee = () => {
+  return (
+    <div className="service-admin-container">
+      <ServicesAdminPage />
+    </div>
+  );
+};
+
+export default ServiceAdminPagee;
