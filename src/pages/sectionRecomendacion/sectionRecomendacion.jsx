4fc846b47e24ddf8e3cf00d9f3ed00cf47b919f2
rksam <erksg.10.26@gmail.com>
2025-04-11 18:11:20 -0500

    update section recomendable

--- src/pages/sectionRecomendacion/sectionRecomendacion.jsx
+++ src/pages/sectionRecomendacion/sectionRecomendacion.jsx
@@ -1,15 +1,24 @@
 import styles from "../../styles/SectionRecomen.module.css";
+import image1 from "../../assets/image/exfoliantedelabios.jpeg";
+import image2 from "../../assets/image/aceitecaden.jpeg";
+import image3 from "../../assets/image/aceitegirasol.jpeg";
+import image4 from "../../assets/image/aceitedemasaje.jpeg";
+import image5 from "../../assets/image/aceitedeargan.jpeg";
+import image6 from "../../assets/image/exfoliantecorporal.jpeg";
+import image7 from "../../assets/image/aguaderosas.jpeg";
+import image8 from "../../assets/image/salesdepies.jpeg";
+
 
 
 const products = [
-    { id: 1, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/exfoliantedelabios.jpeg'},
-    { id: 2, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/aceitecaden.jpeg'},
-    { id: 3, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/aceitegirasol.jpeg'},
-    { id: 4, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/aceitedemasaje.jpeg' },
-    { id: 5, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/aceitedeargan.jpeg'},
-    { id: 6, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/exfoliantecorporal.jpeg'},
-    { id: 7, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/aguaderosas.jpeg'},
-    { id: 8, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: '/../src/assets/image/salesdepies.jpeg' },
+    { id: 1, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image1 },
+    { id: 2, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image2 },
+    { id: 3, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image3 },
+    { id: 4, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image4 },
+    { id: 5, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image5 },
+    { id: 6, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image6 },
+    { id: 7, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image7 },
+    { id: 8, name: 'Exfoliante de labios', category: 'Cosméticos', price: 'S/20.00', rating: '4.8', image: image8 },
 ];
 
 
@@ -22,9 +31,9 @@ const sectionRecomendacion = () => {
                 ultimas novedades
             </p>
             <div className={styles.grid}>
-            {products.map((product) => (
+            {products?.map((product) => (
                     <div key={product.id} className={styles.cardproduc}>
-                        <img src={product.image} alt={product.name} className={styles.imageproduc} />
+                        <img src={product.image}  className={styles.imageproduc} width='50px' />
                         <div className={styles.infoproduc}>
                             <span className={styles.categoryproduc}>{product.category}</span>
                             <h3 className={styles.nameproduc}>{product.name}</h3>
@@ -34,7 +43,7 @@ const sectionRecomendacion = () => {
                     </div>
                 ))}
                     </div>
-                       <a href="/products"><button className={styles.buttonprod}>Ver más productos</button></a>
+                        <a href="/products"><button className={styles.buttonprod}>Ver más productos</button></a>
                     </div>
     );
 
