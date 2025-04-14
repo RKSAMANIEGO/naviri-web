db381dc364d2c1605dd4d9256377d8b71142f038
Joshua Alvarez Herrera <joshuayherrera@gmail.com>
2025-04-14 17:11:49 -0500

    add: Descriptions on Products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import SectionPrincipal from "../../pages/sectionPrincipal/SectionPrincipal";
 import SectionCategorias from "../sectionCategorias/sectionCategorias";
-import SectionRecomendacion from "../../pages/sectionRecomendacion/sectionRecomendacion";
 import Sectionaboutushome from "../sectionAboutUshome/Sectionaboutushome";
 import Sectionaboutus from "../sectionAboutUs/sectionaboutus";
 import Sectionservices from "../sectionServices/sectionservices";
@@ -9,28 +8,6 @@ import SectionBlog from "../sectionBlog/SectionBlog";
 export const Home = () => {
 
    return (
-/*
-   <>
-      <SectionPrincipal/>
-      <hr className=" text-gray-50"/>
-      <div className="max-full mx-auto">
-      <Sectionaboutushome/>
-      <Sectionaboutus/>
-      <SectionCategorias/>
-      <Sectionservices/>
-      <SectionRecomendacion/>   
-      
-
-         <div className="w-full text-end my-4">
-            <a href="/blog"><button className=" p-2 rounded-full cursor-pointer">Explorar</button></a>
-         </div>
-            <div className="grid md:grid-cols-4 gap-8">
-            {blogsMock.map((blog) => (
-            <BlogCard key={blog.id} blog={blog} />
-            ))}   
-            </div>
-         </div> 
-*/
       <>
          <SectionPrincipal/>
          <hr className=" text-gray-50"/>
@@ -38,8 +15,7 @@ export const Home = () => {
             <Sectionaboutushome/>
             <Sectionaboutus/>
             <SectionCategorias/>
-            <Sectionservices/>
-            <SectionRecomendacion/>   
+            <Sectionservices/> 
             <SectionBlog/>    
          </div> 
 
