f2992e463ad4df00e556f45747dbf683c460163b
Joshua Alvarez Herrera <joshuayherrera@gmail.com>
2025-04-16 12:10:23 -0500

    � refactor: Moving central elements to core

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './core/styles/global.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CartProvider } from './context/CartContext'
