0cc069546c091ce462d8e27413879b5baad13953
rksam <erksg.10.26@gmail.com>
2025-05-12 15:37:58 -0500

    refactor style animation section new products,promotions and details products

--- src/features/products/components/ProductDetails.jsx/ProductDetails.jsx
+++ src/features/products/components/ProductDetails.jsx/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { productByName } from '../../services/productsApi';
 import { EllipsisOutlined, LeftOutlined, LoadingOutlined, RightOutlined, ScissorOutlined, ShoppingCartOutlined } from '@ant-design/icons';
